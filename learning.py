87a4a909e4887eba8f891a97261a182bc215d03d
Rohan Kumar <rohankumar1234590@gmail.com>
2021-12-28 19:55:42 +0530

    new stuffs

--- learning.py
+++ learning.py
@@ -1,12 +1,26 @@
 print("this is file one")
 print("hiiiii")
-print("hellosso numbers")
-
+print("hellosso numsbers rohan")
+x="hello";
+y="bye";
+z=x +" "+ y;
+print(z );
+a=""
 num1 = 1.545
 num2 = 6.323
 
 # Add two numbers
 sum = num1 + num2
+def hello():
+    print("my name us");
+    for x in "rohan":
+        print(x);
+    
 
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+hello();
+
+txt = "The best things in life are free!"
+if "free" not in txt:
+  print("Yes, 'free' is present.")
